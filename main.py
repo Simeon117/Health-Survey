c1816f6ebf818635c8ed31259d480c755388975a
SimeonPres <>
2021-05-18 22:25:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+names = []
+
+class QuizStarter:
+  def __init__(self, parent):
+    background_color="Honeydew"
+
+
+if __name__ == "__main__":
+  root = Tk()
+  root.title("Health Survey")
+  root.mainloop()
